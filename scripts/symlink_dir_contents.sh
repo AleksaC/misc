08b317f3be4f051e1ccff2bd8563704972074eb8
AleksaC <aleksacukovic1@gmail.com>
2020-07-02 23:36:12 +0200

    Add symlink_dir_contents.sh

---
+++ scripts/symlink_dir_contents.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+wd=$(pwd)
+
+if [ ! -d $2 ]; 
+then
+    echo "Cannot find $2"
+else
+    cd $2;
+    
+    if [ ! -d $1 ];
+    then
+        echo "$1 needs to be relative to $2, not $wd"
+        exit 1
+    fi
+
+    for f in $(ls -d $1/*); 
+    do 
+        dest="$(pwd)/$(basename $f)"
+        echo "Linking $f to $dest"
+        ln -s $f $dest
+    done
+fi
