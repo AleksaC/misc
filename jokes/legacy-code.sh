b1809a0306a9d506d5d73eedaccd9e475ca662e1
AleksaC <aleksacukovic1@gmail.com>
2020-09-07 23:00:18 +0200

    Implement legacy-code.sh

--- jokes/legacy-code.sh
+++ jokes/legacy-code.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env/ bash
+#!/usr/bin/env bash
 
 # This script squashes all commits into "legacy code" commit and force pushes it to master
 
 set -eou pipefail
 
+git reset $(git rev-list HEAD | tail -n 1)
+git add .
+git commit --amend -m "Legacy code"
 git push -f
